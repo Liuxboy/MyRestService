cc4daa3143204c6a73c0d6dd11bf903694d6ef4c
liuchundong1 <liuchundong2008@gmail.com>
2017-03-23 22:24:00 +0800

    update

--- src/main/java/com/github/liuxboy/rest/hello/PostGreet.java
+++ src/main/java/com/github/liuxboy/rest/hello/PostGreet.java
@@ -11,8 +11,22 @@ import java.io.Serializable;
  */
 public class PostGreet implements Serializable {
     private static final long serialVersionUID = -7279670488824737721L;
+    private Long id;
     private String name;
-    private String phoneNo;
+    private String phone;
+    private String sex;
+    private String address;
+    private String code;
+    private Long did;
+    private Long pid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -22,11 +36,51 @@ public class PostGreet implements Serializable {
         this.name = name;
     }
 
-    public String getPhoneNo() {
-        return phoneNo;
+    public String getAddress() {
+        return address;
     }
 
-    public void setPhoneNo(String phoneNo) {
-        this.phoneNo = phoneNo;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Long getDid() {
+        return did;
+    }
+
+    public void setDid(Long did) {
+        this.did = did;
+    }
+
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
     }
 }
